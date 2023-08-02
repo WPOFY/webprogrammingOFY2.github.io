b7a8363796164647570367066d5d4a0004f8d15b
WPOFY <141234464+WPOFY@users.noreply.github.com>
2023-08-02 20:58:28 +0700

    Add files via upload

---
+++ Yol/Yol.js
@@ -0,0 +1,10 @@
+function func(){
+    var Name = document.getElementById("Name").value;
+    if (Name == "Jessy Yolanda") {
+        alert("Succesfull ! ")
+        window.location.assign("https://wpofy.github.io/webprogrammingOFY2.github.io/");
+    }
+    else {
+        alert("Unidentified ! ")
+    }
+}
